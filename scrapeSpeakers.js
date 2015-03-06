d2682756f9fc173f330ccff51378a0b91c97ff1d
leeatapigee <ApigeeCorporation@Lee-Greys-Macbook.local>
2015-03-06 00:21:48 -0500

    Use Node SDK to POST speaker entities into speakers collection.

--- scrapeSpeakers.js
+++ scrapeSpeakers.js
@@ -1,5 +1,16 @@
 var request = require('request')
 var cheerio = require('cheerio')
+var usergrid = require('usergrid')
+
+var client = new usergrid.client({
+  orgName: 'devnexus',
+  appName: '2015',
+  authType: usergrid.AUTH_CLIENT_ID,
+  clientId: 'b3U6apHSesK4EeS83ifhCLdAvA',
+  clientSecret: 'b3U632uksMm62PSaNgrbpUkj3kGWxp0',
+  logging: true,
+  buildCurl: true
+})
 
 var urlSpeakers = 'http://devnexus.com/s/speakers'
 
@@ -7,6 +18,7 @@ request(urlSpeakers, function(err, resp, body) {
   var $ = cheerio.load(body),
       $body = $('body'),
       $speakers = $body.find('.speaker-member')
+  var speakerCount = 0
 
   $speakers.each(function(i, item) {
     var speaker = $(item).find('h4').text().trim()
@@ -23,7 +35,27 @@ request(urlSpeakers, function(err, resp, body) {
       bio += $(paragraph).text().trim() + '\n'
       paragraph = $(paragraph).next()
     }
+    ++speakerCount
 
-    console.log(speaker+' '+picture+' '+bio+' '+social.join(' + ')+'\n')
+    // use the usergrid npm module to create the speakers entities
+    var options = {
+      type: 'speakers',
+      name: speaker,
+      speaker: speaker,
+      picture: picture,
+      social: social,
+      bio: bio
+    }
+
+    // TODO the inserts work, but there are async issues with the console output
+    client.createEntity(options, function(err, speakerEntity) {
+      if( err ) {
+        console.log(speakerCount+'  error: '+err)
+      } else {
+        console.log(speakerCount+'  '+speakerEntity.uuid+'='+speakerEntity.name)
+      }
+    })
   })
+
+  console.log(speakerCount+' speakers')
 })
