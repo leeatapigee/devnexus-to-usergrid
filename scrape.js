4eb4887264e98002a24ca9149516b8568f70b39b
leeatapigee <ApigeeCorporation@Lee-Greys-Macbook.local>
2015-03-05 20:29:31 -0500

    Extracts full contents of each session

--- scrape.js
+++ scrape.js
@@ -8,9 +8,32 @@ request(urlSessions, function(err, resp, body) {
   var $ = cheerio.load(body),
       $body = $('body'),
       $sessions = $body.find('.masonryitem')
+  var slotSeparators = [',', '\\|'];
 
   $sessions.each(function(i, item) {
-    var title = $(item).find('.presentation-title').text()
-    console.log(title)
+    var picture = $(item).find('.img-circle').attr('src')
+    var speaker = $(item).find('.img-circle').attr('alt')
+    var title = $(item).find('.presentation-title').text().trim()
+    var desc = $(item).find('.col-xs-12 p').text().trim()
+    // for the session attributes, get the collection of rows
+    var rows = $(item).find('.row .col-xs-9')
+    var track = $(rows[0]).text().trim()
+    var skill = $(rows[1]).text().trim()
+    var slotText = $(rows[2]).text().trim()
+
+    // parse into date, time, room
+    var slotTokens = slotText.split(new RegExp(slotSeparators.join('|'), 'g'))
+    var date = slotTokens[0].trim()
+    var time = slotTokens[1].trim()
+    var room = slotTokens[2].trim()
+
+    // split by commas
+    var tagsTokens = $(rows[3]).text().trim().split(',')
+    var tags = '|'
+    tagsTokens.forEach(function(elem, i, arr){
+      tags += elem.trim() + '|'
+    })
+
+    console.log(title+' '+picture+' '+speaker+' '+'--desc--'+' '+track+' '+skill+' '+date+' '+time+' '+room+' '+' '+tags)
   })
 })
