2afa8850e683f0647c743d355409496e6174fdbf
leeatapigee <ApigeeCorporation@Lee-Greys-Macbook.local>
2015-03-05 23:55:09 -0500

    Use API to POST session entities into sessions collection.

--- scrapeSessions.js
+++ scrapeSessions.js
@@ -1,6 +1,7 @@
 var request = require('request')
 var cheerio = require('cheerio')
 
+
 var urlSessions = 'http://devnexus.com/s/presentations'
 
 request(urlSessions, function(err, resp, body) {
@@ -8,11 +9,14 @@ request(urlSessions, function(err, resp, body) {
       $body = $('body'),
       $sessions = $body.find('.masonryitem')
   var slotSeparators = [',', '\\|'];
+  var sessionCount = 0
+
 
   $sessions.each(function(i, item) {
     var speaker = $(item).find('.img-circle').attr('alt')
     var title = $(item).find('.presentation-title').text().trim()
     var desc = $(item).find('.col-xs-12 p').text().trim()
+
     // for the session attributes, get the collection of rows
     var rows = $(item).find('.row .col-xs-9')
     var track = $(rows[0]).text().trim()
@@ -32,6 +36,35 @@ request(urlSessions, function(err, resp, body) {
       tags += elem.trim() + '|'
     })
 
-    console.log(title+' '+speaker+' '+'--desc--'+' '+track+' '+skill+' '+date+' '+time+' '+room+' '+' '+tags)
+    // use the API to create each session entity
+    var options = {
+      uri: 'https://api.usergrid.com/devnexus/2015/sessions',
+      method: 'POST',
+      auth: {
+        bearer: 'YWMtxoL57sOwEeSMH2_nVDjglAAAAUwRLIqZmpMxFp21w5UQ2KjtBtfq_Kcv-S4'
+      },
+      json: {
+        name: title,
+        title: title,
+        speaker: speaker,
+        description: desc,
+        track: track,
+        skill: skill,
+        date: date,
+        time: time,
+        room: room,
+        tags: tags
+      }
+    }
+
+    request(options, function(err, resp, body) {
+      if( err || resp.statusCode !== 200 ) {
+        console.log(resp.statusCode+': '+err+'\n'+JSON.stringify(body))
+      }
+    })
+
+    ++sessionCount
   })
+
+  console.log(sessionCount+' sessions')
 })
